4bf23f33a681a8f96cae70a6be44d1190497e608
galvarez <guillaume.alvarez@gmail.com>
2016-03-04 22:22:27 +0100

    clearer layout

--- js/components/ResultScreen.jsx
+++ js/components/ResultScreen.jsx
@@ -29,6 +29,8 @@ var ResultScreen = React.createClass({
     },
 
     render: function() {
+        var dead  = this.state.dead;
+        var success = dead.fate == dead.expects;
         return (
           <div className="resultScreen" >
             <div className="modal fade" id="resultScreenModal" tabIndex="-1" role="dialog" aria-labelledby="resultScreenLabel">
@@ -36,10 +38,10 @@ var ResultScreen = React.createClass({
                 <div className="modal-content">
                   <div className="modal-header">
                     <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h2 className="modal-title" id="resultScreenLabel">{this.state.dead.fate == this.state.dead.expects ? "You chose wisely!" : "You chose poorly!"}</h2>
+                    <h2 className="modal-title" id="resultScreenLabel">{success ? "You chose wisely!" : "You chose poorly!"}</h2>
                   </div>
                   <div className="modal-body">
-                    <p className="lead"><strong>This dead soul expected {this.state.dead.expects}: {this.state.dead.says}</strong></p>
+                    <p className="lead"><strong>This dead soul expected {dead.expects}: "{dead.says}"</strong></p>
                   </div>
                   <div className="modal-footer">
                     <button type="button" className="btn btn-primary" data-dismiss="modal">Click anywhere to continue</button>
